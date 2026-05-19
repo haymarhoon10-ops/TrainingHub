28bf1fc4a5a8c4a4fcfb35b994a9b1ea1b290804
Sayed Ali Almusawi <202304831@student.polytechnic.bh>
2026-05-19 23:44:29 +0300

    last controller batch

---
+++ TrainingHub.Mvc/Controllers/NotificationsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TrainingHub.Data;
+using TrainingHub.Models;
+
+namespace TrainingHub.Mvc.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        private readonly TrainingHubDbContext _context;
+
+        public NotificationsController(TrainingHubDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notifications
+        public async Task<IActionResult> Index()
+        {
+            var trainingHubDbContext = _context.Notifications.Include(n => n.Instructor).Include(n => n.Trainee);
+            return View(await trainingHubDbContext.ToListAsync());
+        }
+
+        // GET: Notifications/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications
+                .Include(n => n.Instructor)
+                .Include(n => n.Trainee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+
+        // GET: Notifications/Create
+        public IActionResult Create()
+        {
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "Bio");
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email");
+            return View();
+        }
+
+        // POST: Notifications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Message,Type,CreatedAt,IsRead,TraineeId,InstructorId")] Notification notification)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(notification);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "Bio", notification.InstructorId);
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email", notification.TraineeId);
+            return View(notification);
+        }
+
+        // GET: Notifications/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications.FindAsync(id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "Bio", notification.InstructorId);
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email", notification.TraineeId);
+            return View(notification);
+        }
+
+        // POST: Notifications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Message,Type,CreatedAt,IsRead,TraineeId,InstructorId")] Notification notification)
+        {
+            if (id != notification.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notification);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotificationExists(notification.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstructorId"] = new SelectList(_context.Instructors, "Id", "Bio", notification.InstructorId);
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email", notification.TraineeId);
+            return View(notification);
+        }
+
+        // GET: Notifications/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notifications
+                .Include(n => n.Instructor)
+                .Include(n => n.Trainee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+
+        // POST: Notifications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+
+            if (notification == null)
+                return NotFound();
+
+            try
+            {
+                _context.Notifications.Remove(notification);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to delete notification.");
+                return View("Delete", notification);
+            }
+        }
+
+        private bool NotificationExists(int id)
+        {
+            return _context.Notifications.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TrainingHub.Mvc/Controllers/PaymentsController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TrainingHub.Data;
+using TrainingHub.Models;
+
+namespace TrainingHub.Mvc.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly TrainingHubDbContext _context;
+
+        public PaymentsController(TrainingHubDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payments
+        public async Task<IActionResult> Index()
+        {
+            var trainingHubDbContext = _context.Payments.Include(p => p.Enrollment).Include(p => p.Trainee);
+            return View(await trainingHubDbContext.ToListAsync());
+        }
+
+        // GET: Payments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments
+                .Include(p => p.Enrollment)
+                .Include(p => p.Trainee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public IActionResult Create()
+        {
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "Id", "AttendanceStatus");
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TraineeId,EnrollmentId,AmountPaid,PaidAt,PaymentMethod,ReferenceNumber,Notes")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "Id", "AttendanceStatus", payment.EnrollmentId);
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email", payment.TraineeId);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "Id", "AttendanceStatus", payment.EnrollmentId);
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email", payment.TraineeId);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TraineeId,EnrollmentId,AmountPaid,PaidAt,PaymentMethod,ReferenceNumber,Notes")] Payment payment)
+        {
+            if (id != payment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentExists(payment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "Id", "AttendanceStatus", payment.EnrollmentId);
+            ViewData["TraineeId"] = new SelectList(_context.Trainees, "Id", "Email", payment.TraineeId);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.Payments
+                .Include(p => p.Enrollment)
+                .Include(p => p.Trainee)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var payment = await _context.Payments.FindAsync(id);
+
+            if (payment == null)
+                return NotFound();
+
+            try
+            {
+                _context.Payments.Remove(payment);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to delete payment because related records exist.");
+                return View("Delete", payment);
+            }
+        }
+
+        private bool PaymentExists(int id)
+        {
+            return _context.Payments.Any(e => e.Id == id);
+        }
+    }
+}
