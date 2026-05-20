4e33196546b7d700276e7af5443043898f9e8be6
haymarhoon10-ops <haymarhoon10@gmail.com>
2026-05-20 18:32:47 +0300

    progresss is working

--- TrainingHub.Mvc/Controllers/CertificationTracksController.cs
+++ TrainingHub.Mvc/Controllers/CertificationTracksController.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TrainingHub.Data;
 using TrainingHub.Models;
-using TrainingHub.Mvc.Models;
 
 namespace TrainingHub.Mvc.Controllers
 {
@@ -20,41 +14,32 @@ namespace TrainingHub.Mvc.Controllers
             _context = context;
         }
 
-        // GET: CertificationTracks
         public async Task<IActionResult> Index()
         {
             return View(await _context.CertificationTracks.ToListAsync());
         }
 
-        // GET: CertificationTracks/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var certificationTrack = await _context.CertificationTracks
-    .Include(ct => ct.CertificationTrackCourses)
-        .ThenInclude(ctc => ctc.Course)
-    .FirstOrDefaultAsync(m => m.Id == id);
+                .Include(ct => ct.CertificationTrackCourses)
+                    .ThenInclude(ctc => ctc.Course)
+                .FirstOrDefaultAsync(ct => ct.Id == id);
+
             if (certificationTrack == null)
-            {
                 return NotFound();
-            }
 
             return View(certificationTrack);
         }
 
-        // GET: CertificationTracks/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: CertificationTracks/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Description,IsActive")] CertificationTrack certificationTrack)
@@ -65,36 +50,29 @@ namespace TrainingHub.Mvc.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(certificationTrack);
         }
 
-        // GET: CertificationTracks/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var certificationTrack = await _context.CertificationTracks.FindAsync(id);
+
             if (certificationTrack == null)
-            {
                 return NotFound();
-            }
+
             return View(certificationTrack);
         }
 
-        // POST: CertificationTracks/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,IsActive")] CertificationTrack certificationTrack)
         {
             if (id != certificationTrack.Id)
-            {
                 return NotFound();
-            }
 
             if (ModelState.IsValid)
             {
@@ -106,38 +84,31 @@ namespace TrainingHub.Mvc.Controllers
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!CertificationTrackExists(certificationTrack.Id))
-                    {
                         return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+
+                    throw;
                 }
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(certificationTrack);
         }
 
-        // GET: CertificationTracks/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var certificationTrack = await _context.CertificationTracks
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .FirstOrDefaultAsync(ct => ct.Id == id);
+
             if (certificationTrack == null)
-            {
                 return NotFound();
-            }
 
             return View(certificationTrack);
         }
 
-        // POST: CertificationTracks/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -150,8 +121,7 @@ namespace TrainingHub.Mvc.Controllers
             if (certificationTrack == null)
                 return NotFound();
 
-            if (certificationTrack.CertificationTrackCourses.Any() ||
-                certificationTrack.Certificates.Any())
+            if (certificationTrack.CertificationTrackCourses.Any() || certificationTrack.Certificates.Any())
             {
                 ModelState.AddModelError("", "Cannot delete this certification track because it is connected to courses or certificates.");
                 return View("Delete", certificationTrack);
@@ -170,61 +140,61 @@ namespace TrainingHub.Mvc.Controllers
             }
         }
 
-        // GET: CertificationTracks/Progress?traineeId=1&trackId=1
         public async Task<IActionResult> Progress(int? traineeId, int? trackId)
         {
             if (traineeId == null || trackId == null)
             {
-                return BadRequest("Trainee ID and Track ID are required.");
+                ViewBag.ErrorMessage = "Trainee ID and Track ID are required.";
+                return View();
             }
 
-            var trainee = await _context.Trainees.FindAsync(traineeId);
+            var trainee = await _context.Trainees
+                .FirstOrDefaultAsync(t => t.Id == traineeId);
+
             if (trainee == null)
             {
-                return NotFound("Trainee not found.");
+                ViewBag.ErrorMessage = "Trainee not found.";
+                return View();
             }
 
-            var certificationTrack = await _context.CertificationTracks
+            var track = await _context.CertificationTracks
                 .Include(ct => ct.CertificationTrackCourses)
                     .ThenInclude(ctc => ctc.Course)
                 .FirstOrDefaultAsync(ct => ct.Id == trackId);
 
-            if (certificationTrack == null)
+            if (track == null)
             {
-                return NotFound("Certification Track not found.");
+                ViewBag.ErrorMessage = "Certification Track not found.";
+                return View();
             }
 
-            // Get total required courses in the track
-            int totalCourses = certificationTrack.CertificationTrackCourses.Count;
+            var requiredCourseIds = track.CertificationTrackCourses
+                .Select(ctc => ctc.CourseId)
+                .ToList();
 
-            // Get completed courses for the trainee
-            var completedCourses = await _context.Enrollments
+            var completedCourseIds = await _context.Enrollments
+                .Include(e => e.CourseSession)
                 .Where(e =>
                     e.TraineeId == traineeId &&
                     e.Status == "Completed" &&
-                    e.ResultStatus == "Pass" &&
-                    certificationTrack.CertificationTrackCourses
-                        .Select(ctc => ctc.CourseId)
-                        .Contains(e.CourseSession!.CourseId))
+                    e.ResultStatus == "Pass")
                 .Select(e => e.CourseSession!.CourseId)
                 .Distinct()
-                .CountAsync();
+                .ToListAsync();
 
-            // Calculate progress percentage
-            decimal progressPercentage = totalCourses > 0
-                ? Math.Round((decimal)completedCourses / totalCourses * 100, 2)
-                : 0;
+            int totalRequired = requiredCourseIds.Count;
+            int completedCount = requiredCourseIds.Count(id => completedCourseIds.Contains(id));
 
-            var viewModel = new CertificationProgressViewModel
-            {
-                TraineeName = trainee.FullName,
-                CertificationTrackName = certificationTrack.Name,
-                CompletedCourses = completedCourses,
-                TotalRequiredCourses = totalCourses,
-                ProgressPercentage = progressPercentage
-            };
+            double progressPercentage = totalRequired == 0
+                ? 0
+                : (double)completedCount / totalRequired * 100;
 
-            return View(viewModel);
+            ViewBag.TraineeName = trainee.FullName;
+            ViewBag.CompletedCount = completedCount;
+            ViewBag.TotalRequired = totalRequired;
+            ViewBag.ProgressPercentage = Math.Round(progressPercentage, 0);
+
+            return View(track);
         }
 
         private bool CertificationTrackExists(int id)
